c90c3066099d7e5dc860e18699f6d1c9b7e118d2
Cameron Roe <1614348+ninjasort@users.noreply.github.com>
2024-01-31 14:27:57 +0100

    Fix type issues

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -13,7 +13,7 @@ import { Button } from '@/app/ui/button';
 import { createInvoice } from '@/app/lib/actions';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
-  const initialState = { message: null, errors: {} };
+  const initialState = { message: '', errors: {} };
   const [state, dispatch] = useFormState(createInvoice, initialState);
   return (
     <form action={dispatch}>
